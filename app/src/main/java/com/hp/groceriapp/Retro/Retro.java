d472167e979da12705a04609128b4a4d3f9b6a0f
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2020-05-13 07:14:47 +0530

    timeout fix

--- app/src/main/java/com/hp/groceriapp/Retro/Retro.java
+++ app/src/main/java/com/hp/groceriapp/Retro/Retro.java
@@ -1,5 +1,8 @@
 package com.hp.groceriapp.Retro;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,8 +10,15 @@ public class Retro {
     // create a method which returns our api interface class
     public Apis getApi()
     {
+        //handle time outs
+        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .build();
         //build retrofit object
         Retrofit retrofit=new Retrofit.Builder()
+                .client(okHttpClient)
                 .baseUrl("http://srishti-systems.info/projects/groceryShop/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
